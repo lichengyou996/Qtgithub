fb612b184cd5637856f74ee5fd6661cb48156076
Administrator <1369460747@qq.com>
2022-10-10 11:33:21 +0800

    修改main函数 printf("hello world")

--- Qtgithub/main.cpp
+++ Qtgithub/main.cpp
@@ -1,10 +1,11 @@
 #include "Qtgithub.h"
 #include <QtWidgets/QApplication>
-
+#include <stdio.h>
 int main(int argc, char *argv[])
 {
 	int i = 11;
 	int j = i + 11;
+	printf("hello world");
     QApplication a(argc, argv);
     Qtgithub w;
     w.show();
