1fb69362a740aa1cb5409fc0c3a9842a2b994504
Administrator <1369460747@qq.com>
2022-10-10 11:21:46 +0800

    i=1;

--- Qtgithub/main.cpp
+++ Qtgithub/main.cpp
@@ -3,15 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-	int j = 1 - 3;
-	int k = j;
-	int kk = 110;
-	int kkk = 1100;
-	int  jjjj = 11;
-	int jjj;
-	int kkkkk;
-	int kkkkkkjjj;
-	int jjj = j + 12;
+	int i = 1;
     QApplication a(argc, argv);
     Qtgithub w;
     w.show();
